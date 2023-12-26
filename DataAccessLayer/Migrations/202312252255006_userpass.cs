22f3c0484b5ceb7c785fce91849ccad3026d4bcf
Bahadir <bahadirbunul5@gmail.com>
2023-12-26 11:35:41 +0300

    Add project files.

---
+++ DataAccessLayer/Migrations/202312252255006_userpass.cs
@@ -0,0 +1,28 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class userpass : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "Name", c => c.String());
+            AlterColumn("dbo.Users", "SurName", c => c.String());
+            AlterColumn("dbo.Users", "Email", c => c.String());
+            AlterColumn("dbo.Users", "UserName", c => c.String());
+            AlterColumn("dbo.Users", "Password", c => c.String());
+            AlterColumn("dbo.Users", "RePassword", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "RePassword", c => c.String(nullable: false, maxLength: 10));
+            AlterColumn("dbo.Users", "Password", c => c.String(nullable: false, maxLength: 10));
+            AlterColumn("dbo.Users", "UserName", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Users", "Email", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Users", "SurName", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Users", "Name", c => c.String(nullable: false, maxLength: 50));
+        }
+    }
+}
---
+++ SayFurniture/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SayFurniture")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SayFurniture")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16e77419-6130-43b3-b3a9-16ec8064c305")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
